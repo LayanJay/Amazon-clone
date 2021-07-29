d5c9b37ea13f05bba15323954a571d921d44aaac
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-07-29 09:48:24 +0530

    add: product section

---
+++ components/ProductSection.jsx
@@ -0,0 +1,18 @@
+import Product from './Product'
+
+const ProductSection = ({ products, title }) => {
+  return (
+    <>
+      <h2 className='font-semibold text-lg sm:text-xl capitalize mb-3'>
+        {title}
+      </h2>
+      <section className='grid grid-cols-1 grid-rows-1 md:grid-cols-2 lg:grid-cols-4 gap-4 place-items-center mb-8'>
+        {products.map((product, i) => (
+          <Product key={i} product={product} />
+        ))}
+      </section>
+    </>
+  )
+}
+
+export default ProductSection
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,27 +2,53 @@ import CarouselComp from '../components/Carousel'
 import Layout from '../components/common/Layout'
 import Container from '../components/common/Container'
 import Categories from '../modules/Categories'
+import ProductSection from '../components/ProductSection'
 
 export const getStaticProps = async () => {
   const categories = await fetch(
     'https://fakestoreapi.com/products/categories'
   ).then((res) => res.json())
 
+  const allProducts = await fetch(
+    'https://fakestoreapi.com/products?limit=12'
+  ).then((res) => res.json())
+
   return {
     props: {
       categories,
+      allProducts,
     },
   }
 }
 
-const Home = ({ categories }) => {
+const Home = ({ categories, allProducts }) => {
   return (
     <Layout>
       <Container>
         <section className='relative'>
           <CarouselComp />
-          <div className='absolute top-1/2 sm:px-4 w-full'>
-            <Categories categories={categories} />
+          <div className='absolute top-2/3 sm:top-1/2 sm:px-4 w-full bg-gradient-to-t from-gray-100 via-gray-100 to-transparent'>
+            <div>
+              <Categories categories={categories} />
+            </div>
+            <div>
+              <ProductSection
+                title="Shop Men's Clothing"
+                products={allProducts.slice(0, 4)}
+              />
+            </div>
+            <div>
+              <ProductSection
+                title='Shop From Top Sellers'
+                products={allProducts.slice(4, 8)}
+              />
+            </div>
+            <div>
+              <ProductSection
+                title='Shop Electronic Items'
+                products={allProducts.slice(8, 12)}
+              />
+            </div>
           </div>
         </section>
       </Container>
