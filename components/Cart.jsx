e7a6317cf47f7ee3c92d21fe1beed2d2a12d4f55
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-08-07 21:30:38 +0530

    add: handle checkout function

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from 'react'
+import { useCartState } from '../context/cart'
+import { useAuth } from '../hooks/auth'
+import { addToCart } from '../lib/firebase/cart'
 import Button from './common/Button'
 import LineItem from './LineItem'
 
 const Cart = ({ items }) => {
+  const user = useAuth()
+  const state = useCartState()
   const [total, setTotal] = useState(0)
   useEffect(() => {
     let value = 0
@@ -11,6 +16,14 @@ const Cart = ({ items }) => {
       setTotal(value)
     }
   }, [items])
+
+  const handleCheckout = async () => {
+    const obj = { ...state, checkoutToken: `check_${user.uid}` }
+    await addToCart(obj, user)
+    alert(
+      'Your checkout token has been added to the database. Since the payments are handled externaly, there no gateway to handle payments. But you are free to implement it and test it out.'
+    )
+  }
 
   return (
     <div className='shadow-sm rounded-md border-2 border-gray-200 p-3 sm:p-4'>
@@ -24,7 +37,9 @@ const Cart = ({ items }) => {
         <p className='font-light text-3xl sm:text-4xl mb-5'>
           {total.toFixed(2)}
         </p>
-        <Button padding='py-1 px-10'>Checkout</Button>
+        <Button onClick={handleCheckout} padding='py-1 px-10'>
+          Checkout
+        </Button>
       </div>
     </div>
   )
