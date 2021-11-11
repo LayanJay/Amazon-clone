9ea7bd61f57bf4133d01bfc4c2c000cf491c024c
LayanJay <layanjayasinghe2@gmail.com>
2021-11-11 10:07:14 +0530

    revert: hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const { merchant_id, order_id, payment_id, payhere_amount, captured_amount, status_code, status_message } = req
-  res.status(200).json({ merchant_id, order_id, payment_id, payhere_amount, captured_amount, status_code, status_message })
+  res.status(200).json({ name: 'John Doe' })
 }
