6d3d63d0182dce90414b081492a6235ac51121ee
LayanJay <layanjayasinghe2@gmail.com>
2021-11-11 09:06:21 +0530

    testing route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const { merchant_id, order_id, payment_id, payhere_amount, captured_amount, status_code, method, status_message } = req
+  res.status(200).json({ merchant_id, order_id, payment_id, payhere_amount, captured_amount, status_code, method, status_message })
 }
